4c8f079505814010cb7e900fbb64411a6ba74732
Kirill Saidov <kirillsaidovd@yahoo.com>
2021-11-14 13:32:41 +0600

    incorrect result, fixing

--- 01-50/p2.c
+++ 01-50/p2.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
+// NOTE: DO NOT FORGET TO LINK MATH LIB: -lm
+
 // Binet's formula for calculating Fibonacci numbers using the Golden Ratio Phi
-int binetsFibonacci(const int n);
+long binetsFibonacci(const int n);
 
 // The Golden Ratio
 const float phi = (1 + sqrt(5))/2;
 
 int main(void) {
 	int i = 2;		// start from fibonacci = 1
-	int sum = 0;
-	int fibnum = 0;
+	long sum = 0;
+	long fibnum = 0;
 
-	while((fibnum = binetsFibonacci(i++)) < 400) {
-		if(!(fibnum & 1)) { // sum even fibonacci numbers
+	while((fibnum = binetsFibonacci(i++)) < 4e+6) { // less than 4 million
+		if(/*!(fibnum & 1)*/ fibnum%2 == 0) { // sum even fibonacci numbers
 			sum += fibnum;
+			printf("%d: %d\n", i-1, fibnum);
 		}
 	}
 
-	printf("Sum of even fibonacci numbers below 4e+9: %d\n", sum);
+	printf("Sum of even fibonacci numbers below 4e+6: %ld\n", sum);
 	
 	return 0;
 }
 
-int binetsFibonacci(const int n) {
+long binetsFibonacci(const int n) {
 	return round((pow(phi, n) - (1/pow(-phi, n))) / (2*phi - 1));
 }
