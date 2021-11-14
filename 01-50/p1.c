38e2e4473a185ed83c0bfc99cecbf73ee644f5ce
kirill.saidov <kirill.saidov.d@gmail.com>
2021-11-14 09:07:23 +0600

    p1 solved

---
+++ 01-50/p1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+// boundary (we iterate up to and equals to 999, or below 1000)
+#define N 999
+
+// solutions
+int bruteforceSolution();
+int analyticalSolution();
+
+// main
+int main(void) {
+	printf("Bruteforce solution: %d\n\n", bruteforceSolution()); // 233 168 
+	printf("Analytical solution: %d\n\n", analyticalSolution()); // 
+	return 0;
+}
+
+// Calculates the sum of multiples of 3 and 5 below 1000 (slower)
+int bruteforceSolution() {
+	int sum = 0;
+
+	for(int i = 0; i < N+1; i++) {
+		if(i%3 == 0 || i%5 == 0) {
+			sum += i;
+		}
+	}
+
+	return sum;
+}
+
+int analyticalSum(int n) {
+	return n*(n + 1)/2;
+}
+
+// Calculates the sum of multiples of 3 and 5 below 1000 analytically (faster)
+int analyticalSolution() {
+	// multiples
+	int m3 = 3;
+	int m5 = 5;
+	int m15 = 15;
+
+	// upper lowest bound of multiples
+	int M3 = (int)(N/m3);
+	int M5 = (int)(N/m5);
+	int M15 = (int)(N/m15);
+
+	// sum of multiples of 3, 5, 15 below 1000
+	int sum3 = m3 * analyticalSum(M3);
+	int sum5 = m5 * analyticalSum(M5);
+	int sum15 = m15 * analyticalSum(M15);
+
+	return sum3 + sum5 - sum15;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
