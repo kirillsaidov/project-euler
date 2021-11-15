6c0ad012761a047736ae67c99ad373bfbf78c27c
kirill.saidov <kirill.saidov.d@gmail.com>
2021-11-15 08:19:16 +0600

    p2 solved

--- 01-50/p2.c
+++ 01-50/p2.c
@@ -1,31 +1,44 @@
 #include <stdio.h>
 #include <math.h>
 
-// NOTE: DO NOT FORGET TO LINK MATH LIB: -lm
+// NOTE: link -lm when compiling
 
-// Binet's formula for calculating Fibonacci numbers using the Golden Ratio Phi
-long binetsFibonacci(const int n);
+long binetsSolution(void);
 
 // The Golden Ratio
-const float phi = (1 + sqrt(5))/2;
+const double phi = 1.6180339887; // (1 + sqrt(5)) / 2
 
 int main(void) {
+	printf("Sum of even fibonacci numbers below 4e+6\n");
+	printf("using Binet's formula: %ld\n", binetsSolution());
+
+	return 0;
+}
+
+// Binet's formula for calculating Fibonacci numbers using the Golden Ratio
+long binetsFormula(const int n) {
+	return round((pow(phi, n) - (1/pow(-phi, n))) / (2*phi - 1));
+}
+
+long binetsSolution(void) {
 	int i = 2;		// start from fibonacci = 1
 	long sum = 0;
 	long fibnum = 0;
 
-	while((fibnum = binetsFibonacci(i++)) < 4e+6) { // less than 4 million
-		if(/*!(fibnum & 1)*/ fibnum%2 == 0) { // sum even fibonacci numbers
+	while((fibnum = binetsFormula(i++)) < 4e+6) { // less than 4 million
+		if(!(fibnum & 1)) { // sum even fibonacci numbers
 			sum += fibnum;
-			printf("%d: %d\n", i-1, fibnum);
 		}
 	}
 
-	printf("Sum of even fibonacci numbers below 4e+6: %ld\n", sum);
-	
-	return 0;
+	return sum;
 }
 
-long binetsFibonacci(const int n) {
-	return round((pow(phi, n) - (1/pow(-phi, n))) / (2*phi - 1));
-}
+
+
+
+
+
+
+
+
